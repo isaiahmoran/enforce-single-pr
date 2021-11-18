b5f7b050bf7335a8085c147f0c595027c06f57e3
isaiahmoran <35080753+isaiahmoran@users.noreply.github.com>
2021-11-18 12:06:53 -0500

    Better error handling (#13)
    * error handling
    
    * error
    
    * fix
    
    * number
    
    * fix

--- src/Action.ts
+++ src/Action.ts
@@ -10,12 +10,16 @@ export class Action {
     let filtered = pullRequests.filter((pr) => pr.user?.login == this.userName)
 
     if (filtered.length > 1){
-      let newest = filtered[0].head.ref
+      let newest = filtered[0].number
       filtered.splice(0, 1)
       for (let i = 0; i < filtered.length; i++){
-        this.api.createComment(filtered[i].number, `Closing PR as a newer one from ${this.userName} exists at ${newest}`)
-        this.api.closePullRequest(filtered[i].number)
-        this.api.deletePullRequestBranch(filtered[i])
+        try {
+          this.api.createComment(filtered[i].number, `Closing PR as it is superceeded by #${newest}`)
+          this.api.closePullRequest(filtered[i].number)
+          this.api.deletePullRequestBranch(filtered[i])
+        } catch (e) {
+          core.warning(`An error occurred closing PR # ${filtered[i].number}, moving on to the next PR.`)
+        }
       }
     }
   }
