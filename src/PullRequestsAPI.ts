5a0dd2857677838014954d89fdc63cf452cdddf4
Isaiah Moran <moran@whoop.com>
2021-11-18 11:45:20 -0500

    error

--- src/PullRequestsAPI.ts
+++ src/PullRequestsAPI.ts
@@ -48,7 +48,7 @@ class GithubPullRequestAPI implements PullRequestAPI {
       repo: this.repo,
       issue_number: issue_number,
       state: 'closed'
-    }).catch(error => console.log(`An error occurred closing the pull request #${issue_number}`))
+    }).catch(error => console.log(`An error occurred closing the pull request #${issue_number}`, error))
       .then(() => console.log("Continuing"))
   }
 
@@ -58,7 +58,7 @@ class GithubPullRequestAPI implements PullRequestAPI {
       repo: this.repo,
       issue_number: issue_number,
       body: body
-    }).catch(error => console.log(`An error occurred creating a comment on pull request #${issue_number}`))
+    }).catch(error => console.log(`An error occurred creating a comment on pull request #${issue_number}`, error))
       .then(() => console.log("Continuing"))
   }
 
@@ -67,7 +67,7 @@ class GithubPullRequestAPI implements PullRequestAPI {
       owner: this.owner,
       repo: this.repo,
       ref: `heads/${pullRequest.head.ref}`
-    }).catch(error => console.log(`An error occurred deleting branch associated with request #${pullRequest.number} at heads/${pullRequest.head.ref}`))
+    }).catch(error => console.log(`An error occurred deleting branch associated with request #${pullRequest.number} at heads/${pullRequest.head.ref}`, error))
       .then(() => console.log("Continuing"))
   }
 }
