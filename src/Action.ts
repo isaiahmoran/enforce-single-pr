88c6c9b00080c1439c950fe7bb0ab3cc5e2e5c5c
Isaiah Moran <moran@whoop.com>
2021-11-17 21:02:59 -0500

    wire up yaml

--- src/Action.ts
+++ src/Action.ts
@@ -1,10 +1,20 @@
 import {PullRequestAPI} from './PullRequestsAPI'
 
 export class Action {
-  constructor(private api: PullRequestAPI, private userName: string) {
-  }
+  constructor(private api: PullRequestAPI, private userName: string) {}
 
   async perform() {
-    // do the thing
+    let pullRequests = await this.api.listPullRequests()
+    let filtered = pullRequests.filter((pr) => pr.user?.login == this.userName)
+
+    if (filtered.length > 1){
+      let newest = filtered[0].head.ref
+      filtered.splice(0, 1)
+      for (let i = 0; i < filtered.length; i++){
+        this.api.createComment(filtered[i].number, `Closing PR as a newer one from ${this.userName} exists at ${newest}`)
+        this.api.closePullRequest(filtered[i].number)
+        this.api.deletePullRequestBranch(filtered[i])
+      }
+    }
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,20 @@
-async function run() {
+import * as github from '@actions/github'
+import {createPullRequestAPI} from './PullRequestsAPI'
+import {Action} from './Action'
 
+async function run() {
+  let action = initializeAction()
+  await action.perform()
+}
+
+function getToken(): string {
+  return process.env.GITHUB_API_TOKEN ?? ""
+}
+
+function initializeAction(): Action {
+  const git = github.getOctokit(getToken())
+  let api = createPullRequestAPI(git, "whoopinc", "android")
+  return new Action(api, "fuck")
 }
 
 run()
